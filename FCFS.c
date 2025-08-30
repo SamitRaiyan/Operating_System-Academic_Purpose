d9e30d5587341efacbdd619ae8f24b8b7c64c200
SamitRaiyan <samitr247@gmail.com>
2025-08-31 01:48:27 +0600

    close to final one

--- FCFS.c
+++ FCFS.c
@@ -1,73 +1,76 @@
-// Experiment 1: First-Come, First-Served (FCFS) Scheduling 
-# include <stdio.h>
+// First Come First Serve (FCFS) Scheduling
+#include <stdio.h>
 
-struct pcb{
-    int pid,at,bt,tat;
-    // pid: Process ID
-    // at: Arrival Time
-    // bt: Burst Time
-    // tat: Turnaround Time
+struct pcb {
+    int pid, at, bt, ct, tat, wt;
+    // pid = Process ID
+    // at  = Arrival Time
+    // bt  = Burst Time
+    // ct  = Completion Time
+    // tat = Turnaround Time
+    // wt  = Waiting Time
 };
 
-void pline(int x)
-{
+// Line printing function (void pline(int x))
+void pline(int x) {
     int i;
-    for(i = 0; i < x; i++)
-        printf("-");
+    for(i = 0; i < x; i++) printf("-");
     printf("\n");
 }
 
-void main()
-{
-    int i,num,j;
-    float avg = 0.0, sum = 0.0;
+int main() {
+    int num, i, j, time = 0;  // এখানে num ভ্যারিয়েবলে মোট process সংখ্যা থাকবে
+    float avg_tat = 0, avg_wt = 0;
     struct pcb p[10], temp;
 
-    // Input process details
-    printf("Enter the total number of processes: ");
+    printf("Enter total number of processes: ");
     scanf("%d", &num);
 
-    // Input Arrival Time and Burst Time
-    for(i = 0; i < num; i++)
-    {
-       printf("Enter the Arrival Time and Burst Time of process %d: ", i + 1);
-    scanf("%d %d", &p[i].at, &p[i].bt);
-    p[i].pid = i + 1;
+    // Input AT and BT
+    for(i = 0; i < num; i++) {
+        printf("Enter Arrival Time and Burst Time of Process %d: ", i+1);
+        scanf("%d %d", &p[i].at, &p[i].bt);
+        p[i].pid = i+1;
     }
 
-    // Sorting processes based on Arrival Time
-    for(i = 0; i < num - 1; i++)
-    {
-        for(j = 0; j < num - i - 1; j++)
-        {
-            if(p[j].at > p[j + 1].at)
-            {
+    // Sort by Arrival Time
+    for(i = 0; i < num-1; i++) {
+        for(j = 0; j < num-i-1; j++) {
+            if(p[j].at > p[j+1].at) {
                 temp = p[j];
-                p[j] = p[j + 1];
-                p[j + 1] = temp;
+                p[j] = p[j+1];
+                p[j+1] = temp;
             }
         }
     }
 
-    // Calculating Turnaround Time
-    for(i = 0; i < num; i++)
-    {
-        sum = sum + p[i].bt;
-        p[i].tat = sum;
-    }
-    sum = 0;
-    pline(50);
-    printf(" Process ID \t Arrival Time \t Burst Time \t Turnaround Time \n");
-    pline(50);
-    for(i = 0; i < num; i++)
-    {
-        printf("      %d      \t      %d      \t     %d      \t       %d       \n",
-               p[i].pid, p[i].at, p[i].bt, p[i].tat);
-               sum += p[i].tat;
-    }
-    pline(50);
-    avg = sum /(float) num;
-    printf("Total Turnaround Time: %.2f\n", sum);
-    printf("Average Turnaround Time: %.2f\n", avg);
+    // Calculate CT, TAT, WT
+    time = p[0].at;
+    for(i = 0; i < num; i++) {
+        if(time < p[i].at)  // যদি CPU idle থাকে
+            time = p[i].at;
 
+        time += p[i].bt;
+        p[i].ct = time;
+        p[i].tat = p[i].ct - p[i].at; // TAT = CT - AT
+        p[i].wt  = p[i].tat - p[i].bt; // WT = TAT - BT
+
+        avg_tat += p[i].tat;
+        avg_wt  += p[i].wt;
+    }
+
+    // Output
+    pline(60);
+    printf("PID\tAT\tBT\tCT\tTAT\tWT\n");
+    pline(60);
+    for(i = 0; i < num; i++) {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",
+               p[i].pid, p[i].at, p[i].bt, p[i].ct, p[i].tat, p[i].wt);
+    }
+    pline(60);
+
+    printf("Average Turnaround Time = %.2f\n", avg_tat/num);
+    printf("Average Waiting Time = %.2f\n", avg_wt/num);
+
+    return 0;
 }
